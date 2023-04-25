b58a003735b929d8174e558362779d937f126ea1
Hynson <ehynson@ucsc.edu>
2023-04-24 20:14:01 -0700

    Lab 4 Update

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,34 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock Paper Scissors
+// Author: Wes Babb <gababb@ucsc.edu> & Elijah Hynson <hynson@ucsc.edu>
+// Date: 4/21/23
+/*
+  Import Random
+  Create a list that has rock, paper, scissors
+  Create a variable that is equal to the random choice from rock paper scissors. The computer will use this variable against the player
+
+  Ask for user input
+  User will answer by inputting rock paper or scissors.
+  If input does not match the accepted choices, ask the user to choose again 
+    Else create a variable that is equal to the user’s input
+
+  If user’s choice equals computer’s choice, print Tie
+    Else if user’s choice equals Rock:
+      If computer’s choice equals Scissors
+        Print “Rock beats Scissors, You Win!”
+      Else computer’s choice equals Paper
+        Print “Paper beats Rock, You Lose!”
+    Else if user’s choice equals Paper:
+      If computer’s choice equals Rock:
+        Print “Paper beats Rock, You Win!”
+      Else computer’s choice equals Scissors:
+        Print “Scissors beats Paper, You Lose!”
+    Else if user’s choice equals Scissors:
+      If computer’s choice equals Paper:
+        Print “Scissors beats Paper, You Win!”
+      Else computer’s choice equals Rock:
+        Print “Rock beat Scissors, You Lose!”
+*/
+
 
 // Constants
 
