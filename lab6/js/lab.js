30494fc919bd4f4036b70a536ebcea1f90c0e18e
Hynson <ehynson@ucsc.edu>
2023-05-01 18:16:49 -0700

    Lab 6 Update
    Full Lab 6 webpage

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,18 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Arrays and Objects (featuring Main Modes of Transportation)
+// Author: Elijah Hynson <ehynson@ucsc.edu> and Wes Babb <gababb@ucsc.edu> 
+// Date: 5/1/2023
 
-// Constants
+const myTransport = ["Mercedes c320", "Black Low Top Air Force One's with Marbled Soles", "UCSC Loop Bus"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+var myMainRide = {
+  make: "Mercedes",
+  model: "c320",
+  color: "Black" ,
+  year: 2003,
+  age: function() {
+      return 2023 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("My Main Modes of Transportation: " + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
